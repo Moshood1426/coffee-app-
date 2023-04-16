4528c474366aa9c52700c1d9afd9a22c5e1cc815
Moshood1426 <moshood.yemi10@gmail.com>
2023-04-16 18:55:54 -0400

    svg files added to project

--- src/pages/coffee-store/[id].js
+++ src/pages/coffee-store/[id].js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
-
+import styles from "../../styles/coffee-store.module.css";
 import coffeStoreData from "../../../coffee-stores.json";
+import Head from "next/head";
+import Image from "next/image";
 
 export async function getStaticProps(staticProps) {
   const params = staticProps.params;
@@ -15,22 +17,91 @@ export async function getStaticProps(staticProps) {
 }
 
 export function getStaticPaths() {
+  const paths = coffeStoreData.map((item) => ({
+    params: {
+      id: item.id.toString(),
+    },
+  }));
+
   return {
-    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
-    fallback: false,
+    paths: [...paths],
+    fallback: true,
   };
 }
 
-const CoffeeStore = () => {
+const CoffeeStore = ({ coffeeStore }) => {
   const router = useRouter();
 
-  if(router.isFallback) {
-    return <div>Loading...</div>
+  if (router.isFallback) {
+    return <div>Loading...</div>;
   }
 
+  const { address, name, neighbourhood, imgUrl } = coffeeStore;
+
   return (
-    <div>
-      <Link href="">Back to home</Link>
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+        <meta name="description" content={`${name} coffee store`} />
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">← Back to home</Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={
+              imgUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
+        </div>
+
+        <div className={cls("glass", styles.col2)}>
+          {address && (
+            <div className={styles.iconWrapper}>
+              <Image
+                src="/static/icons/places.svg"
+                width="24"
+                height="24"
+                alt="places icon"
+              />
+              <p className={styles.text}>{address}</p>
+            </div>
+          )}
+          {neighbourhood && (
+            <div className={styles.iconWrapper}>
+              <Image
+                src="/static/icons/nearMe.svg"
+                width="24"
+                height="24"
+                alt="near me icon"
+              />
+              <p className={styles.text}>{neighbourhood}</p>
+            </div>
+          )}
+          <div className={styles.iconWrapper}>
+            <Image
+              src="/static/icons/star.svg"
+              width="24"
+              height="24"
+              alt="star icon"
+            />
+            <p className={styles.text}>{votingCount}</p>
+          </div>
+
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up vote!
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
